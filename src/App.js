979196ca902a860b9de05d248c42b86d5232a9ee
occludedarp <cmrnmr@gmail.com>
2019-10-07 22:16:28 -0400

    finished MVP

--- src/App.js
+++ src/App.js
@@ -5,19 +5,10 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  //lions
+  
   let [homeCurrentScore, homeUpdateScore] = useState(32)
 
-  // const homeScoreKeeper = homeCurrentScore => {
-  //   homeUpdateScore(homeCurrentScore + 6);
-  // };
-
   let [awayCurrentScore, awayUpdateScore] = useState(32)
-
-  // const awayScoreKeeper = awayCurrentScore => {
-  //   awayUpdateScore(awayCurrentScore);
-  // };
-
 
   return (
     <div className="container">
@@ -41,11 +32,11 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => homeUpdateScore(homeCurrentScore + 6)}>Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => homeUpdateScore(homeCurrentScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => homeUpdateScore(homeCurrentScore + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => awayUpdateScore(awayCurrentScore + 6)}>Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={() => awayUpdateScore(awayCurrentScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => awayUpdateScore(awayCurrentScore + 3)}>Away Field Goal</button>
         </div>
       </section>
